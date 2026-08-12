031bcc8eef2c348689a853e4dd79e28c0afe1c2a
Gabriel Calle <111298148+GatoNegro5@users.noreply.github.com>
2026-08-11 21:33:55 -0500

    fix: elevate global compileSdkVersion to 35 to support Android 15 permissions API

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,7 +23,7 @@ subprojects {
                 if (namespace == null) {
                     namespace = project.group.toString()
                 }
-                compileSdkVersion(34)
+                compileSdkVersion(35)
             }
         }
     }
